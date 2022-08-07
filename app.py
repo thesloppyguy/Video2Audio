0d45384204b9bfd3434fe8db18d4daa6361d7b84
thesloppyguy <sahilmandal@outlook.com>
2022-08-07 19:09:44 +0530

    FINAL

--- app.py
+++ app.py
@@ -3,22 +3,25 @@ import moviepy.editor as mp
 import os
 
 link = argv[2]
-path = argv[3] 
-flag=0
+path = argv[3]
+flag = 0
 
 if '-o' in argv[1]:
-    flag=0
+    flag = 0
 if '-m' in argv[1]:
-    flag=1
+    flag = 1
 
-def convert(name='',link=link):
-    clip = mp.VideoFileClip(link)
-    clip.audio.write_audiofile((path+'\\'+name+'.mp3'))
 
-if flag==0:
-    temp=path.split('/')[-1].split('.')[0]
-    convert(temp)
-    pass
+def convert(link=link):
+    name = link.split('/')[-1].split('.')
+    if name[1] != "mp3":
+
+        clip = mp.VideoFileClip(link)
+        clip.audio.write_audiofile((path+'/'+name[0]+'.mp3'))
+
+
+if flag == 0:
+    convert()
 else:
     for i in os.listdir(link):
-        convert(i,link+'\\'+i,)
+        convert(link + '/'+i)
